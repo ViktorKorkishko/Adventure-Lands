e6e5ee895aff2aa9a2286f7017a3580eac1309ac
ViktorKorkishko <ViktorKorkishko@users.noreply.github.com>
2019-12-06 20:58:43 +0200

    Stucturising Part 1

---
+++ Assets/Scripts/Player Scripts/HeartManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartManager : MonoBehaviour
+{
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite halfFullHeart;
+    public Sprite emptyHeart;
+    public FloatValue heartsContainers;
+    public FloatValue playerCurrentHealth;
+
+    void Start()
+    {
+        InitHearts();
+    }
+
+    public void InitHearts()
+    {
+        for(int i = 0; i < heartsContainers.initialValue; i++)
+        {
+            hearts[i].gameObject.SetActive(true);
+            hearts[i].sprite = fullHeart;
+        }
+    }
+
+    public void UpdateHearts()
+    {
+        float tempHealth = playerCurrentHealth.RuntimeValue / 2;
+        for(int i = 0; i < heartsContainers.initialValue; i++)
+        {
+            if(i <= tempHealth - 1)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else if(i >= tempHealth)
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+            else
+            {
+                hearts[i].sprite = halfFullHeart;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerHit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHit : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("breakable"))
+        {
+            other.GetComponent<Pot>().Smash();
+        }
+    }
+}
