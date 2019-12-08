e376661d79f943c96f8879698f6066fe4d27a284
ViktorKorkishko <ViktorKorkishko@users.noreply.github.com>
2019-12-08 15:54:42 +0200

    Dungeon Setup Ready

---
+++ Assets/Scripts/Objects/Door.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DoorType
+{
+    key,
+    enemy,
+    button
+}
+
+public class Door : Interactable
+{
+    [Header("Door vaiables")]
+    public DoorType thisDoorType;
+    public bool open;
+    public Inventory playerInventory;
+    public SpriteRenderer doorSprite;
+    public BoxCollider2D physicsCollider;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (playerInRange && thisDoorType == DoorType.key)
+            {
+                if (playerInventory.numberOfKeys > 0)
+                {
+                    playerInventory.numberOfKeys--;
+                    Open();
+                }
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    public void Open()
+    {
+        doorSprite.enabled = false;
+        open = true;
+        physicsCollider.enabled = false;
+    }
+
+    public void Close()
+    {
+
+    }
+}
