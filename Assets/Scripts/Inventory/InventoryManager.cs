a9af73b1986c715f7b1e5b17f5a45d56ac95d8fc
ViktorKorkishko <ViktorKorkishko@users.noreply.github.com>
2020-03-05 23:55:18 +0200

    Inventory Part 3

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InventoryManager : MonoBehaviour
+{
+    [Header("Inventory information")]
+    public PlayerInventory playerInventory;
+    [SerializeField] private GameObject blankInventorySlot;
+    [SerializeField] private GameObject inventoryPanel;
+    [SerializeField] private TextMeshProUGUI descriptionText;
+    [SerializeField] private GameObject useButton;
+
+    public void SetTextAndButton(string description, bool buttonActive)
+    {
+        descriptionText.text = description;
+        if (buttonActive)
+        {
+            useButton.SetActive(true);
+        }
+        else
+        {
+            useButton.SetActive(false);
+        }
+    }
+
+    void MakeInventorySlot()
+    {
+        if (playerInventory)
+        {
+            for (int i = 0; i < playerInventory.myInventory.Count; i++)
+            {
+                GameObject temp = Instantiate(blankInventorySlot, inventoryPanel.transform.position, Quaternion.identity);
+                temp.transform.SetParent(inventoryPanel.transform);
+                temp.transform.localScale = new Vector3(1, 1, 1);
+                InventorySlot newSlot = temp.GetComponent<InventorySlot>();
+                if (newSlot)
+                {
+                    newSlot.Setup(playerInventory.myInventory[i], this);
+                }
+            }
+        }
+    }
+
+    void Start()
+    {
+        MakeInventorySlot();
+        SetTextAndButton("", false);
+    }
+
+    void Update()
+    {
+
+    }
+}
